05a6462775466854f7f6c846683576359dce1b0d
kans1904 <karen_navarrete89@hotmail.com>
2021-01-08 16:33:03 -0500

    primer commit

---
+++ js/script.js
@@ -0,0 +1,106 @@
+function nombreImagen() {
+var nombreimagen= prompt ("podemos clasificar por medio del"); 
+if (nombreimagen=="color, tamaño y forma") {
+    alert("es correcto")
+} else {
+    alert("es incorrecto")
+}
+}
+
+function Tamaño() {
+    var nombretamaño= prompt ("cual es el tamaño de los arboles"); 
+    if (nombretamaño=="pequeño, grande y mediano") {
+        alert("es correcto")
+    } else {
+        alert("es incorrecto")
+    }
+    }
+
+    function nombreColores() {
+        var nombrecolores = prompt("Qué colores hay en la clasificación");
+        if (nombrecolores == "rojo, verde y amarillo") {
+            alert("es correcto");
+        } else {
+            alert("es incorrecto")
+        }
+    }
+        function forma() {
+            var nombreforma= prompt ("las figuras con las que se esta clasificando son"); 
+            if (nombreforma=="circulo, cuadrado y triangulo") {
+                alert("es correcto")
+            } else {
+                alert("es incorrecto")
+            }
+        }
+
+
+        function empaquesrigidos() {
+            var nombreempaques= prompt ("los cucharones ubicados atras del sillón son de:"); 
+            if (nombreempaques=="metal") {
+                alert("es correcto")
+            } else {
+                alert("es incorrecto")
+            }
+        }
+
+        function plastico() {
+            var nombreeplastico= prompt ("cuantas botellas de plastico hay"); 
+            if (nombreeplastico=="2") {
+                alert("es correcto")
+            } else {
+                alert("es incorrecto")
+            }
+        }
+        function carton() {
+            var nombreecarton= prompt ("¿Qué alimentos y liquidos  hay empacados en cartón?"); 
+            if (nombreecarton=="huevos, leche y jugo") {
+                alert("es correcto")
+            } else {
+                alert("es incorrecto")
+            }
+        }
+
+        function liquidos() {
+            var nombreliquidos= prompt ("¿Qué liquido proviene de la leche?"); 
+            if (nombreliquidos=="yogurth") {
+                alert("es correcto")
+            } else {
+                alert("es incorrecto")
+            }
+        }
+
+        function litro() {
+            var nombrelitro= prompt ("¿Qué objeto representa el litro?"); 
+            if (nombrelitro=="jarra") {
+                alert("es correcto")
+            } else {
+                alert("es incorrecto")
+            }
+        }
+
+        function empaquesflexibles() {
+            var nombrelitro= prompt ("¿de qué material son los producros que no pertenecen al tipo de empaques?"); 
+            if (nombrelitro=="metal y plastico") {
+                alert("es correcto")
+            } else {
+                alert("es incorrecto")
+            }
+        }
+
+        function isotipos() {
+            var nombreisotipos= prompt ("¿Qué representa la m?"); 
+            if (nombreisotipos=="Mcdonals") {
+                alert("es correcto")
+            } else {
+                alert("es incorrecto")
+            }
+        }
+        function logotipos() {
+            var nombreisotipos= prompt ("¿Qué representa la f?"); 
+            if (nombreisotipos=="facebook") {
+                alert("es correcto")
+            } else {
+                alert("es incorrecto")
+            }
+        }
+        
